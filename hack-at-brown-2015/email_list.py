acf372f6c0b9a23b8fb07c4cc7aa45e4055daee1
nate-parrott <nateparro2t@gmail.com>
2014-09-24 12:14:47 -0400

    Better splash page, w/ signup that works

--- hack-at-brown-2015/email_list.py
+++ hack-at-brown-2015/email_list.py
@@ -1,5 +1,6 @@
 import webapp2
 from google.appengine.ext import ndb
+import json
 
 class EmailListEntry(ndb.Model):
   email = ndb.StringProperty()
@@ -8,12 +9,14 @@ class EmailListEntry(ndb.Model):
   @classmethod
   def add_email(cls, email_address):
 		existing = EmailListEntry.query(EmailListEntry.email == email_address).fetch()
-		if existing == None:
+		if existing == []:
 			entry = EmailListEntry(email=email_address)
 			entry.put()
+			return True
+		else:
+			return False
 
 class SignUpForUpdatesHandler(webapp2.RequestHandler):
 	def post(self):
-		email = self.request.get('email')
-		EmailListEntry.add_email(email)
-		self.redirect('/#gotcha')
+		EmailListEntry.add_email(self.request.get('email'))
+		self.response.write(json.dumps({"success": True}))
---
+++ hack-at-brown-2015/static/js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+	var $signUpForUpdatesForm = $("#sign-up-for-updates");
+	$signUpForUpdatesForm.submit(function(e) {
+		e.preventDefault();
+		$signUpForUpdatesForm.find('input[type=email]').blur().val("");
+		$signUpForUpdatesForm.addClass('loading');
+		$.ajax({
+			url: $signUpForUpdatesForm.attr('action'),
+			data: $signUpForUpdatesForm.serialize(),
+			method: 'POST',
+			success: function() {
+				$signUpForUpdatesForm.removeClass('loading').addClass('signed-up');
+				$signUpForUpdatesForm.find('input[type=email]').attr('placeholder', "✓   we'll keep you posted")
+			}
+		})
+	})
+	$signUpForUpdatesForm.find('input[type=email]').focus(function() {
+		$(this).attr('placeholder', 'email@school.edu');
+	}).blur(function() {
+		$(this).attr('placeholder', 'subscribe for updates');
+	})
+});
