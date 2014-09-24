4f49b6729305db99ba1b3fd90ac2b0cca6942ccb
nate-parrott <nateparro2t@gmail.com>
2014-09-24 17:23:47 -0400

    sends registration confirmation email now

--- hack-at-brown-2015/registration.py
+++ hack-at-brown-2015/registration.py
@@ -1,6 +1,8 @@
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import ndb
 import json
+from send_email import send_email
+from template import template
 
 class Hacker(ndb.Model):
 	name = ndb.StringProperty()
@@ -21,4 +23,8 @@ class RegistrationHandler(blobstore_handlers.BlobstoreUploadHandler):
 		if len(resume_files) > 0:
 			hacker.resume = resume_files[0].key()
 		hacker.put()
+		
+		email_html = template("emails/confirm_registration.html", {"name": hacker.name.split(" ")[0]})
+		send_email(recipients=[hacker.email], subject="You've applied to Hack@Brown!", html=email_html)
+		
 		self.response.write(json.dumps({"success": True}))
