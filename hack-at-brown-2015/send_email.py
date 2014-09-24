ed33d22ed2fc1cc5614763ce9b33c94e6fcc1df1
nate-parrott <nateparro2t@gmail.com>
2014-09-24 17:49:29 -0400

    email sucks

--- hack-at-brown-2015/send_email.py
+++ hack-at-brown-2015/send_email.py
@@ -1,10 +1,10 @@
 from google.appengine.api import mail
 import pynliner
-from html2text import HTML2Text
+import html2text
 
 def send_email(html, subject, recipients):
 	html = pynliner.fromString(html) # pynliner turns css stylesheets into inline styles, because gmail + other clients disallow style sheets
-	plain_text = HTML2Text(html) # because we _must_ provide a plain-text alternative message body
+	plain_text = html2text.html2text(html) # because we _must_ provide a plain-text alternative message body
 	for recip in recipients:
 		mail.send_mail(sender="HackAtBrown Robot <robot@hackatbrown.org>", to=recip, subject=subject, body=plain_text, html=html)
 		print "MESSAGE:"
