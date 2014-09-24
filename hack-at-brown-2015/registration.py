e4ca74dfb6307b45887cd106edd216dad14f6113
nate-parrott <nateparro2t@gmail.com>
2014-09-24 15:14:27 -0400

    better animations and other stuff i guess

--- hack-at-brown-2015/registration.py
+++ hack-at-brown-2015/registration.py
@@ -1,4 +1,23 @@
-import webapp2
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import ndb
 
-class RegistrationHandler(webapp2.RequestHandler):
-	pass
+class Hacker(ndb.Model):
+	name = ndb.StringProperty()
+	school = ndb.StringProperty()
+	year = ndb.StringProperty()
+	email = ndb.StringProperty()
+	shirt_size = ndb.StringProperty()
+	dietary_restrictions = ndb.StringProperty()
+	resume = ndb.BlobKeyProperty()
+	date = ndb.DateTimeProperty(auto_now_add=True)
+
+class RegistrationHandler(blobstore_handlers.BlobstoreUploadHandler):
+	def post(self):
+		hacker = Hacker()
+		for key in ['name', 'school', 'year', 'email', 'shirt_size', 'dietary_restrictions']:
+			setattr(hacker, key, self.request.get(key))
+		resume_files = self.get_uploads('resume')
+		if len(resume_files) > 0:
+			hacker.resume = resume_files[0].key()
+		hacker.put()
+		self.redirect('/#thanks')
