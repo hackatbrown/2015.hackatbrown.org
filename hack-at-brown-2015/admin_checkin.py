f0efb9c3db2d413e4ad32a37431925708d554c26
skortchmark9 <samuel_kortchmar@brown.edu>
2014-11-29 23:37:34 -0500

    More extensible

--- hack-at-brown-2015/admin_checkin.py
+++ hack-at-brown-2015/admin_checkin.py
@@ -3,24 +3,40 @@ import webapp2
 import json
 from template import template
 from registration import Hacker
-from google.appengine.ext import ndb
 from google.appengine.api import memcache
 
 cache_time = 6 * 10
+hackerFormat = ['name', 'email']
+divider = ' - '
+
 
 class CheckinPageHandler(webapp2.RequestHandler):
-    def get(self):
-        hacker_names = map(lambda hacker: hacker.name, get_hackers_to_be_checked())
 
-        response = template("checkin_page.html", {"hacker_names": json.dumps(hacker_names)})
+    def get(self):
+
+        #Can't use statements in lambdas :(
+        def formatter(hacker):
+            formattedString = ''
+            first = True
+            for key in hackerFormat:
+                if not first:
+                    formattedString += divider
+
+                formattedString += getattr(hacker, key)
+                first = False
+            return formattedString
+
+        source = map(formatter, get_hackers_to_be_checked())
+
+        response = template(
+            "checkin_page.html", {"source": json.dumps(source)})
 
         self.response.write(response)
-        self.response.headers["Content-Type"] = "text/HTML"
 
     def post(self):
-        #TODO: improve - use keys.
-        hackerName = self.request.get('name')
-        hacker = Hacker.query(Hacker.name == hackerName).fetch(1)[0]
+        name_email = self.request.get('name/email').split(divider)
+        hacker = Hacker.query(Hacker.name == name_email[0]
+                              and Hacker.email == name_email[1]).fetch(1)[0]
         hacker.checked_in = True
         hacker.put()
         self.redirect('/admin_checkin')
@@ -34,20 +50,21 @@ def get_hackers_to_be_checked():
         logging.info("Used cache")
         return data
     else:
-        data = Hacker.query(Hacker.rsvpd == True and Hacker.checked_in != True).fetch(projection=[Hacker.name])
+        data = Hacker.query(Hacker.rsvpd == True
+                            and Hacker.checked_in != True).fetch(
+            projection=[Hacker.name, Hacker.email])
         logging.info("Could not use cache")
         if not memcache.add(to_check_in, data, cache_time):
             logging.error('Memcache set failed')
     return data
 
+
 def check_in_hacker(hacker):
     hacker.check_in = True
-    #Do we also use memcache here?
+    # Do we also use memcache here?
     hacker.put()
 
 
 app = webapp2.WSGIApplication([
     ('/admin_checkin', CheckinPageHandler),
 ], debug=True)
-
-
