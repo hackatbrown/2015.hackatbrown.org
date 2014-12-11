0c5a282f7a60813fab15266f3d9193cca11882e0
skortchmark9 <samuel_kortchmar@brown.edu>
2014-12-11 17:13:27 -0500

    Implemented secret page a little - redirect on bad key, show status

--- hack-at-brown-2015/hacker_page.py
+++ hack-at-brown-2015/hacker_page.py
@@ -3,6 +3,26 @@ import template
 import registration
 
 class HackerPageHandler(webapp2.RequestHandler):
-	def get(self, secret):
-		hacker = registration.Hacker.WithSecret(secret)
-		self.response.write(template.template("hacker_page.html", {"hacker": hacker}))
+    def get(self, secret):
+        hacker = registration.Hacker.WithSecret(secret)
+
+        if hacker is None:
+            self.redirect('/')
+
+        status = computeStatus(hacker)
+        self.response.write(template.template("hacker_page.html", {"hacker": hacker, "status": status}))
+
+def computeStatus(hacker):
+    if hacker is None:
+        return "Not Found"
+
+    if hacker.checked_in == True:
+        return "Checked In"
+    elif hacker.rsvpd == True:
+        return "RSVP'd"
+    elif hacker.admitted_email_sent_date != None:
+        return "Registered"
+    elif hacker.waitlist_email_sent_date != None:
+        return "Waitlisted"
+    else:
+        return "Not Currently Admitted"
