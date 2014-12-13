710c82a21f286c5b5e5674a6d491de29789523b7
Chen Ye <blasternt@outlook.com>
2014-12-12 23:17:23 -0500

    restore urls to hackatbrown.org

--- hack-at-brown-2015/send_email.py
+++ hack-at-brown-2015/send_email.py
@@ -6,6 +6,6 @@ def send_email(html, subject, recipients):
 	html = pynliner.fromString(html) # pynliner turns css stylesheets into inline styles, because gmail + other clients disallow style sheets
 	plain_text = html2text.html2text(html) # because we _must_ provide a plain-text alternative message body
 	for recip in recipients:
-		mail.send_mail(sender="HackAtBrown Robot <robot@hackatbrown2015-dev.appspotmail.com>", to=recip, subject=subject, body=plain_text, html=html)
+		mail.send_mail(sender="HackAtBrown Robot <robot@hackatbrown.org>", to=recip, subject=subject, body=plain_text, html=html)
 		print "MESSAGE:"
 		print html.encode('utf-8')
