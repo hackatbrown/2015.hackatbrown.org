52d17f841e96fdcb41e97ac993fc88cb7c835b17
skortchmark9 <samuel_kortchmar@brown.edu>
2014-12-14 02:49:01 -0500

    more ajax

--- hack-at-brown-2015/static/js/hacker_page.js
+++ hack-at-brown-2015/static/js/hacker_page.js
@@ -2,8 +2,8 @@ function saveChange(key, value) {
     secret = window.location.pathname.replace('/secret', '');
     $.ajax({
       url : '/__update_hacker/' + secret,
-      type : 'POST'
-    }
+      type : 'POST',
+    });
 
     http.post('/__update_hacker/' + secret, {key : value}).
     success(function(data, status, headers, config) {
