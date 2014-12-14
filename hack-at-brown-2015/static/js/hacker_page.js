5be57aba5c9771fd3131147900bb7e7bd0082dae
skortchmark9 <samuel_kortchmar@brown.edu>
2014-12-14 03:56:34 -0500

    Began work on loaders

--- hack-at-brown-2015/static/js/hacker_page.js
+++ hack-at-brown-2015/static/js/hacker_page.js
@@ -2,10 +2,14 @@ function saveChange(key, value) {
     secret = window.location.pathname.replace('/secret/', '');
     var data = {};
     data[key] = value;
+    $('#loading').addClass('loading');
     $.ajax({
       url : '/__update_hacker/' + secret,
       type : 'POST',
-      data : JSON.stringify(data)
+      data : JSON.stringify(data),
+      success : function(data, status) {
+        $('#loading').removeClass('loading');
+      }
     });
 
     // success(function(data, status, headers, config) {
