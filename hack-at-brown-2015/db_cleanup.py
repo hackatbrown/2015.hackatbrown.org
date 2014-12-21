b1419ad56ad215247c8073481eef91ccd98e1909
skortchmark9 <samuel_kortchmar@brown.edu>
2014-12-21 12:00:22 -0500

    firs stab at populate db

--- hack-at-brown-2015/db_cleanup.py
+++ hack-at-brown-2015/db_cleanup.py
@@ -2,6 +2,7 @@ import webapp2
 from google.appengine.ext import ndb
 from dashboard import getAllHackers
 from registration import Hacker
+from registration import generate_secret_hacker_with_email
 import json
 from template import template
 from registration import hacker_keys
@@ -49,5 +50,47 @@ class PopulateHandler(webapp2.RequestHandler):
             return self.redirect('/')
 
         for i in range(0, number):
-            hacker = createTestHacker()
-            hacker.put()
+            hacker = createTestHacker(i)
+
+
+def createTestHacker(number):
+    shirts = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+    def prob(): return random.uniform(0.0, 10.0)
+    hacker = Hacker()
+    hacker.name = "Hacker {1}".format(number)
+    hacker.school = "Brown University" if prob() < 3 else "Another University"
+
+    hacker.year = "freshman"
+    if prob() < 3:
+        hacker.year = "sophomore"
+    elif prob() < 3:
+        hacker.year = "junior"
+    elif prob() < 3:
+        hacker.year = "senior"
+
+    hacker.email = "hacker_{1}@{2}.edu".format(number, hacker.school.lower().split(" ")[0])
+    hacker.secret = generate_secret_hacker_with_email(hacker.email)
+
+    hacker.shirt_gen = "M" if prob() < 5 else "W"
+    hacker.shirt_size = random.choice(shirts)
+
+    rawDiet = ["Vegetarian", "Vegan", "Gluten Free", "Kosher", "Lactose Intolerant", "Nuts Allergy", "Treenuts Allergy", "Soy Allergy",
+"Shellfish Allergy", "Corn Allergy", "No Pork", "No Ham", "No Beef", "No Mutton", "Halal", "No Red Meat", "None"]
+    drs = []
+    while prob() < 2:
+        item = random.choice(rawDiet)
+        rawDiet.remove(item)
+        drs.push(item)
+
+    hacker.dietary_restrictions = ','.join(drs)
+
+    tms = []
+    while prob() < 2:
+        teammates.push("hacker_{1}@another.edu".format(randint(0, number)))
+
+    hacker.hardware_hack = "yes" if prob() > 8 else "no"
+    hacker.first_hackathon = "yes" if prob() > 7 else "no"
+
+    hacker.put()
+
+    return hacker
