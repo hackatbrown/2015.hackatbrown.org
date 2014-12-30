cfd42a4e089618f375b295846a776bd7909221c5
Will Thompson <toastygoast@toastygoast.local>
2014-12-30 15:37:24 -0500

    can now send emails from files, including teammates

--- hack-at-brown-2015/messages.py
+++ hack-at-brown-2015/messages.py
@@ -35,19 +35,19 @@ class Message(ndb.Model):
 
 	def send_to_email(self, email, template_args={}):
 		# does the actual work of sending
-		print "calling send_to_email"
 		emails = [email]
 		assert self.email_subject, "No email subject provided. Is email unchecked?"
 		if self.audience == 'invited-friends':
-			hacker = Hacker.query(Hacker.email==email).fetch()
+			hacker = Hacker.query(Hacker.email==email).fetch()[0]
 			if hacker.teammates:
 				emails_found = [email.lower() for email in hacker.teammates.split(',')]
-				matching_hackers = yield Hacker.query(Hacker.email.IN(emails_found)).fetch_async()
+				matching_hackers =  Hacker.query(Hacker.email.IN(emails_found)).fetch()
 				emails_already_registered = [h.email for h in matching_hackers]
 				emails = []
 				for email in emails_found:
 					if email not in emails_already_registered:
 						emails.append(email)
+						template_args["invited_by"] = hacker
 		if self.email_from_template:
 			html = template("emails/" + self.email_html + ".html", template_args)
 		else:
@@ -143,9 +143,7 @@ class MessagesDashboardHandler(webapp2.RequestHandler):
 		if self.request.get('test'):
 			recip = self.request.get('test-recipient')
 			if '@' in recip:
-				print "about to send"
 				message.send_to_email(recip)
-				print "done sending?"
 				self.response.write("Sent email")
 			elif len(recip) > 0:
 				message.send_to_phone(recip)
