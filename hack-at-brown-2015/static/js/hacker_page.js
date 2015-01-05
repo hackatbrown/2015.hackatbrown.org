721f2ed68c6edc33faa75e878a8f914f42327003
Chen Ye <blasternt@outlook.com>
2015-01-05 02:40:30 -0600

    scroll to the top on paneswitch

--- hack-at-brown-2015/static/js/hacker_page.js
+++ hack-at-brown-2015/static/js/hacker_page.js
@@ -13,6 +13,7 @@ function setTabActive(tab) {
 
 function switchPanes(paneNumber) {
     var $panes = $(".panes");
+    $('html,body').animate({scrollTop: 0}, 1000);
     $panes.removeClass("in-pane0 in-pane1 in-pane2");
     $panes.addClass("in-pane" + paneNumber);
     $(".pane" + paneNumber).addClass("active");
