58eac24fbdde6b6859c5919d5ea21b1ecf090db6
skortchmark9 <samuel_kortchmar@brown.edu>
2015-01-17 01:28:04 -0400

    set up angular and did the shirt size stuff

--- hack-at-brown-2015/static/js/checkin.js
+++ hack-at-brown-2015/static/js/checkin.js
@@ -1,34 +1,49 @@
-function checkinHacker(id) {
-  $.ajax({
-    type: 'POST',
-    url: '/checkin',
-    data: {'id' : id},
-    success: function(response) {
-      console.log(response);
-    },
-    error: function(error) {
-      console.log('error');
-      console.log(error);
+var checkinApp = angular.module('checkinApp', []).config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('%%').endSymbol('%%');
     }
-  })
-}
+);
 
-function requestMoreInfo(id) {
-  $.ajax({
-    type: 'GET',
-    url: '/checkin/info/' + id,
-    success : function(response) {
-      response = JSON.parse(response);
-      var hacker = response.hacker;
-      console.log(hacker);
-      var missingInfo = response.missingInfo;
-      var check = confirm("Check this person in?");
-      if (check) checkinHacker(id);
 
-    },
-    error: function(error) {
-      console.log('error');
-      console.log(error);
-    }
-  });
-}
+checkinApp.controller('Controller', ['$scope', '$http', function ($scope, $http){
+  $scope.showMissingInfo = false;
+  $scope.showReminders = true;
+  $scope.showStatus = false;
+  $scope.hacker = {};
+
+  $scope.reminders = ['Remind this hacker about food or something.', 'Remind this hacker that travel receipts are due on 1.2.2015'];
+
+  $scope.requestMoreInfo = function() {
+    $.ajax({
+      type: 'GET',
+      url: '/checkin/info/' + $scope.hackerID,
+      success : function(response) {
+        response = JSON.parse(response);
+        $scope.hacker = response.hacker;
+        var missingInfo = response.missingInfo;
+      },
+      error: function(error) {
+        console.log('error');
+        console.log(error);
+      }
+    });
+  }
+
+  $scope.checkinHacker = function() {
+    $.ajax({
+      type: 'POST',
+      url: '/checkin',
+      data: {'id' : $scope.hackerID},
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(error) {
+        console.log('error');
+        console.log(error);
+      }
+    });
+  }
+
+
+}]);
+
+
