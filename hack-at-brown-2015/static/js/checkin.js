8321d27b902507bfa44d4ae99fb9237090a7fc53
skortchmark9 <samuel_kortchmar@brown.edu>
2015-01-19 00:49:29 -0400

    set up angular key binding enter and click

--- hack-at-brown-2015/static/js/checkin.js
+++ hack-at-brown-2015/static/js/checkin.js
@@ -1,3 +1,9 @@
+$(document).on('keyup', function(event) {
+  if (event.which == 13) {
+    angular.element($('#scope')).scope().checkinHacker();
+  }
+});
+
 var checkinApp = angular.module('checkinApp', []).config(function($interpolateProvider){
         $interpolateProvider.startSymbol('%%').endSymbol('%%');
     }
@@ -5,22 +11,28 @@ var checkinApp = angular.module('checkinApp', []).config(function($interpolatePr
 
 
 checkinApp.controller('Controller', ['$scope', '$http', function ($scope, $http){
-  $scope.missingInfo = false;
+  $scope.missingInfo = null;
 
-  $scope.reminders = false;
+  $scope.reminders = null;
 
-  $scope.showStatus = false;
+  $scope.showStatus = null;
   $scope.hacker = {};
 
 
   $scope.requestMoreInfo = function() {
+    if ($scope.hackerID === "") {
+      return;
+    }
+
     $http.get('/checkin/info/' + $scope.hackerID).
       success(function(response) {
         $scope.hacker = response.hacker;
+        console.log($scope.hacker);
+        response.missingInfo = [];
         $scope.missingInfo = response.missingInfo;
         $scope.reminders = ['Remind this hacker about food or something.', 'Remind this hacker that travel receipts are due on 1.2.2015'];
 
-        $scope.showStatus = !$scope.missingInfo;
+        $scope.showStatus = $scope.missingInfo.length === 0;
       }).
       error(function(error) {
         console.log('error');
@@ -29,7 +41,9 @@ checkinApp.controller('Controller', ['$scope', '$http', function ($scope, $http)
   }
 
   $scope.checkinHacker = function() {
-    $http.post('/checkin', {'id' : $scope.hackerID})/
+    console.log('neh');
+    return;
+    $http.post('/checkin', {'id' : $scope.hackerID}).
       success(function(response) {
         console.log(response);
       }).
