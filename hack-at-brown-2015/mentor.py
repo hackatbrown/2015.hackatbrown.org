b0bfdf9429791bb4a16c675ac121f4c0829088a4
skortchmark9 <samuel_kortchmar@brown.edu>
2015-01-27 13:52:27 -0500

    made mentor request models

--- hack-at-brown-2015/mentor.py
+++ hack-at-brown-2015/mentor.py
@@ -1,3 +1,7 @@
+from google.appengine.ext import ndb
+from google.appengine.api import datastore_errors
+import models
+
 maxRating = 5
 minRating = 0
 
@@ -8,20 +12,32 @@ def ratingValidator(prop, value):
     if value < minRating:
         value = minRating
 
+def MentorResponse(ndb.Model):
+    rating = ndb.IntegerProperty(default=None, validator=ratingValidator):
+    request = ndb.KeyProperty(kind=MentorRequest)
+    mentor = ndb.KeyProperty(kind=Mentor)
+    dispatched = ndb.DateTimeProperty(auto_now_add=True)
+    dispatcher = ndb.StringProperty(validator=models.stringValidator)
+    finished == ndb.DateTimeProperty()
+
 class Mentor(ndb.Model):
-    phone = ndb.StringProperty(validator=phoneValidator)
-    email = ndb.StringProperty()
-    tags = ndb.StringProperty(repeated=True)
-    responses = MentorRequestKey(repeated=True)
-    ratings = ndb.IntegerProperty(repeated=True, validator=ratingValidator)
-    avg = ndb.computedProperty(reduce(+, ratings) / self.responses.length)
+    phone = ndb.StringProperty(validator=models.phoneValidator, default=None)
+    email = ndb.StringProperty(validator=models.stringValidator, default=None)
+    tags = ndb.StringProperty(validator=models.stringValidator, repeated=True)
+    responded = ndb.KeyProperty(kind=MentorResponse, repeated=True)
+
+    def getResponded(self):
+        return [key.get() for key in self.responded]
+
+    def computeAvg(self):
+        ratedResponded = [x for x in self.getResponded() if x.rating]
+        return (reduce(lambda x, y: x.rating + y, ratedResponded) / len(ratedResponded))
 
 class MentorRequest(ndb.Model):
-    hacker = Hacker.key(required=True)
-    location = ndb.StringProperty(required=True)
+    requested = ndb.KeyPropery(default=None)
+    location = ndb.StringProperty(default=None)
     sent = ndb.DateTimeProperty(auto_now_add=True)
-    responded = ndb.StructuredProperty(repeated=True)
-    #Time / Mentor / DispatcherEmail
+    responses = ndb.KeyProperty(kind=MentorResponse, repeated=True)
     issue = ndb.TextProperty(required=True)
     tags = ndb.StringProperty(repeated=True)
-
+    solved = ndb.BooleanProperty(default=False)
